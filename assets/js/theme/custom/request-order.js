1f4d5edc1aac4fdd7a4c01e8104d5432a180296d
Volodymyr Kolchin <KolchinVolo@gmail.com>
2021-11-25 03:32:19 +0200

    I added page request-order.html

--- assets/js/theme/custom/request-order.js
+++ assets/js/theme/custom/request-order.js
@@ -4,15 +4,34 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import CustomerData from './reactComponent/orderData'
 import { showAlertModal } from '../global/modal';
+import customerRegister from './gql/customerRegister.gql'
+import initApolloClient from '../global/graphql/client';
+import nod from "nod-validate";
 
 export default class Custom extends PageManager {
     constructor(context) {
         super(context);
+        this.gqlClient = initApolloClient(this.context.storefrontAPIToken);
         this.$addToCartBtn = $('#request-order-btn');
         this.$inputIdOrder = $('#input-request-order')[0];
         this.$addToCartBtn.on('click', () => this.customAddToCartButton());
         this.$totalContainer = $('#request-order')[0];
         this.customerData = null;
+        this.Nod = nod();
+        this.Nod.configure({submit: document.getElementById('request-order-btn'), disableSubmit: true});
+    }
+    onReady() {
+        this.gqlClient.query({
+            query: customerRegister,
+        }).then(res => {
+            if (res.data.customer === null) {
+                $('.error-registered-users').show();
+            } else {
+                $('.container-request-order').show();
+                $('#input-request-order')[0].addEventListener('input', this.inputChange.bind(this));
+            }
+            $('#loadingOverlay').hide();
+        })
     }
     customAddToCartButton () {
         let orderID = this.$inputIdOrder.value;
@@ -37,6 +56,18 @@ export default class Custom extends PageManager {
                 console.log(err);
                 showAlertModal(err);
             });
+    }
+    inputChange (e) {
+        const $input = $(e.target);
+        this.Nod.add([{
+            selector: $input,
+            validate: "integer",
+            errorMessage: `You must enter numbers.`
+        },{
+            selector: $input,
+            validate: "max-length:10",
+            errorMessage: `Your ID order is too long. Maximum characters are 10`
+        }])
     }
 
 }
